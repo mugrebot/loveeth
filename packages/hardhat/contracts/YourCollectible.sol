4b67844aeaabe65ad2b647e765b5797add1dad2c
mugrebot <89887146+mugrebot@users.noreply.github.com>
2022-02-07 18:48:38 -0500

    clone loogies repository affect SVG code to draw rando hearts

---
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -0,0 +1,133 @@
+pragma solidity >=0.6.0 <0.7.0;
+//SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import 'base64-sol/base64.sol';
+
+import './HexStrings.sol';
+import './ToColor.sol';
+//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+
+// GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
+
+contract YourCollectible is ERC721, Ownable {
+
+  using Strings for uint256;
+  using HexStrings for uint160;
+  using ToColor for bytes3;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() public ERC721("Loogies", "LOOG") {
+    // RELEASE THE LOOGIES!
+  }
+
+  mapping (uint256 => bytes3) public color;
+  mapping (uint256 => uint256) public chubbiness;
+  mapping (string => string) public messages;
+
+  uint256 mintDeadline = block.timestamp + 24 hours;
+
+  function mintItem()
+      public
+      returns (uint256)
+  {
+      require( block.timestamp < mintDeadline, "DONE MINTING");
+      _tokenIds.increment();
+
+      uint256 id = _tokenIds.current();
+      _mint(msg.sender, id);
+
+      bytes32 predictableRandom = keccak256(abi.encodePacked( blockhash(block.number-1), msg.sender, address(this), id ));
+      color[id] = bytes2(predictableRandom[0]) | ( bytes2(predictableRandom[1]) >> 8 ) | ( bytes3(predictableRandom[2]) >> 16 );
+      chubbiness[id] = 35+((55*uint256(uint8(predictableRandom[3])))/255);
+
+      return id;
+  }
+
+  function tokenURI(uint256 id) public view override returns (string memory) {
+      require(_exists(id), "not exist");
+      string memory name = string(abi.encodePacked('Loogie #',id.toString()));
+      string memory description = string(abi.encodePacked('This Loogie is the color #',color[id].toColor(),' with a chubbiness of ',uint2str(chubbiness[id]),'!!!'));
+      string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
+
+      return
+          string(
+              abi.encodePacked(
+                'data:application/json;base64,',
+                Base64.encode(
+                    bytes(
+                          abi.encodePacked(
+                              '{"name":"',
+                              name,
+                              '", "description":"',
+                              description,
+                              '", "external_url":"https://burnyboys.com/token/',
+                              id.toString(),
+                              '", "attributes": [{"trait_type": "color", "value": "#',
+                              color[id].toColor(),
+                              '"},{"trait_type": "chubbiness", "value": ',
+                              uint2str(chubbiness[id]),
+                              '}], "owner":"',
+                              (uint160(ownerOf(id))).toHexString(20),
+                              '", "image": "',
+                              'data:image/svg+xml;base64,',
+                              image,
+                              '"}'
+                          )
+                        )
+                    )
+              )
+          );
+  }
+
+  function generateSVGofTokenById(uint256 id) internal view returns (string memory) {
+
+    string memory svg = string(abi.encodePacked(
+      '<svg width="100%" height="100%" viewBox="0 0 900 900" xmlns="http://www.w3.org/2000/svg">',
+        renderTokenById(id),
+      '</svg>'
+    ));
+
+    return svg;
+  }
+
+  // Visibility is `public` to enable it being called by other contracts for composition.
+  function renderTokenById(uint256 id) public view returns (string memory) {
+    string memory render = string(abi.encodePacked(
+        '<g id="head">',
+          '<path id="Bottom" d="M70,279.993C70,279.993 63.297,379.987 70,427.647C85.329,536.631 300.49,820.025 450.016,820.025C599.542,820.025 817.839,533.159 830.014,423.782C835.6,373.594 830.007,280.007 830.007,280.007" style="fill:#', 
+          color[id].toColor(), ';stroke:rgb(0,0,0);stroke-width:5px;"/>',
+        '<path id="Top" d="M449.75,149.777C426.146,149.777 401.744,80.04 249.999,80.001C139.6,79.972 70,169.594 70,279.993C70,450.051 347.857,689.996 450.004,689.996C552.151,689.996 830.007,449.95 830.007,280.007C830.007,169.801 760.231,80.049 650.026,80.049C486.311,80.049 473.355,149.777 449.75,149.777Z" style="fill:#',
+        color[id].toColor(), ';stroke:rgb(0,0,0);stroke-width:5px;"/>',
+        '</g>'
+      ));
+
+    return render;
+  }
+
+  function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+      if (_i == 0) {
+          return "0";
+      }
+      uint j = _i;
+      uint len;
+      while (j != 0) {
+          len++;
+          j /= 10;
+      }
+      bytes memory bstr = new bytes(len);
+      uint k = len;
+      while (_i != 0) {
+          k = k-1;
+          uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+          bytes1 b1 = bytes1(temp);
+          bstr[k] = b1;
+          _i /= 10;
+      }
+      return string(bstr);
+  }
+}
